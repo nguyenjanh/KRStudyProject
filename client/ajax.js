414b5fa19eb8aaa952037e16d0966b5aa3213627
Jason Nguyen <73255669+nguyenjanh@users.noreply.github.com>
2021-11-19 01:06:27 -0500

    Create ajax.js

---
+++ client/ajax.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// path -- string specifying URL to which data request is sent 
+// callback -- function called by JavaScript when response is received                                    
+function ajaxGetRequest(path, callback) {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+          if (this.readyState===4 && this.status ===200) {
+              callback(this.response);
+            }
+    }
+    request.open("GET", path);
+    request.send();
+}
+
+// path -- string specifying URL to which data request is sent
+// data -- JSON blob being sent to the server
+// callback -- function called by JavaScript when response is received
+function ajaxPostRequest(path, data, callback) {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+          if (this.readyState===4 && this.status ===200) {
+              callback(this.response);
+            }
+    }
+    request.open("POST", path);
+    request.send(data);
+}
