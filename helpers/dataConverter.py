6d112140991042b62e403474f87ef4fd6db47226
Jason Nguyen <73255669+nguyenjanh@users.noreply.github.com>
2021-11-19 01:10:08 -0500

    Create dataConverter.py

---
+++ helpers/dataConverter.py
@@ -0,0 +1,10 @@
+import csv
+
+def convert(filename) :
+  with open(filename) as f :
+    dict = {}
+    reader = csv.reader(f)
+    headers = next(f)
+    for line in reader :
+      dict[line[0]] = line[1]
+  return dict
